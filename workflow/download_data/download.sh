4e0598bad95e9abb8f6efea988d2b856c4425fbc
jmoldon <jmoldon@iaa.es>
2023-11-30 19:53:17 +0000

    Fix downloaded destination

--- workflow/download_data/download.sh
+++ workflow/download_data/download.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 # Create the directory if it doesn't exist
-mkdir -p ../../../data2
-
-# Change to the directory
-cd ../../../data2
+mkdir -p ../../../data
 
 # Run the wget commands
 
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=14QJsRopyPpezAjRZ7x57mwQoeBOetNhp' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=14QJsRopyPpezAjRZ7x57mwQoeBOetNhp" -O ../../ZW3.msw_image.fits && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=14QJsRopyPpezAjRZ7x57mwQoeBOetNhp' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=14QJsRopyPpezAjRZ7x57mwQoeBOetNhp" -O ../../../data/ZW3.msw_image.fits && rm -rf /tmp/cookies.txt
 
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1sbOq51-yFzljJbIas_d_YTpBgvLdxW__' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1sbOq51-yFzljJbIas_d_YTpBgvLdxW__" -O ../../ZW3.msw_psf.fits && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1sbOq51-yFzljJbIas_d_YTpBgvLdxW__' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1sbOq51-yFzljJbIas_d_YTpBgvLdxW__" -O ../../../data/ZW3.msw_psf.fits && rm -rf /tmp/cookies.txt
