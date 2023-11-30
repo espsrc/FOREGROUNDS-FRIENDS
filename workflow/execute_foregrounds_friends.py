0666d777890ba92d217dec405984c45897d6e0fd
jmoldon <jmoldon@iaa.es>
2023-11-30 13:37:18 +0000

    Complete working version

--- workflow/execute_foregrounds_friends.py
+++ workflow/execute_foregrounds_friends.py
@@ -1,29 +1,56 @@
 import os
 import shutil
+import subprocess
+import yaml
 import nbformat
 from nbconvert.preprocessors import ExecutePreprocessor
-import yaml
 
-# Read the list of notebooks from a YAML file
+
+# Read the list of tasks from a YAML file
 with open('config/config.yml', 'r') as f:
     config = yaml.safe_load(f)
-    notebooks = config['workflow_steps_notebooks']
+    tasks = config['tasks']
 
 output_dir = 'results'
 os.makedirs(output_dir, exist_ok=True)
+original_dir = os.getcwd()
 
-for notebook_filename in notebooks:
+
+for task in tasks:
     # Copy the entire directory to the results folder
-    notebook_dir = os.path.dirname(notebook_filename)
-    notebook_basename = os.path.basename(notebook_filename)
-    result_dir = os.path.join(output_dir, notebook_dir)
+    task_path = task['path']
+    task_dir = os.path.dirname(task_path)
+    task_basename = os.path.basename(task_path)
+    result_dir = os.path.join(output_dir, task_dir)
     os.makedirs(result_dir, exist_ok=True)
-    shutil.copytree(notebook_dir, result_dir, dirs_exist_ok=True)
+    shutil.copytree(task_dir, result_dir, dirs_exist_ok=True)
 
-    # Execute the notebook in the results folder
-    with open(os.path.join(result_dir, notebook_basename)) as f:
-        nb = nbformat.read(f, as_version=4)
-        ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
-        ep.preprocess(nb, {'metadata': {'path': result_dir}})
-        with open(os.path.join(result_dir, 'executed_' + notebook_basename), 'wt') as f:
-            nbformat.write(nb, f)
+    # Check if the task is a notebook or a Python script
+    if task['type'] == 'notebook':
+        # Execute the notebook in the results folder
+        with open(os.path.join(result_dir, task_basename)) as f:
+            nb = nbformat.read(f, as_version=4)
+            ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
+            ep.preprocess(nb, {'metadata': {'path': result_dir}})
+            with open(os.path.join(result_dir, task_basename), 'wt') as f:
+                nbformat.write(nb, f)
+    elif task['type'] == 'script':
+        # Change the current working directory
+        os.chdir(result_dir)
+        # Execute the Python script in the results folder with command-line arguments
+        args = ['python3', task_basename] + task.get('args', [])
+        print(' '.join(args))
+        subprocess.run(args, check=True)
+        os.chdir(original_dir)
+    if task['type'] == 'bash':
+        # Change the current working directory
+        os.chdir(result_dir)
+        # Execute the bash script in the results folder with command-line arguments
+        args = ['bash', task_basename] + task.get('args', [])
+        print('Executing script:')
+        print(' '.join(args))
+        subprocess.run(args, check=True)
+        os.chdir(original_dir)
+
+
+
